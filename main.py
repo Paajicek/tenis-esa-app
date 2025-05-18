cec165cfcd2f57d3973d26972cf33491ef404728
Paajicek <cihin44@gmail.com>
2025-05-18 23:47:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 # --- Google Sheets setup ---
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
-with open("aceapp-460119-b692dcaf1269.json") as source:
+with open("aceapp-460119-FORMATTED.json") as source:
     creds_dict = json.load(source)
 creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 client = gspread.authorize(creds)
